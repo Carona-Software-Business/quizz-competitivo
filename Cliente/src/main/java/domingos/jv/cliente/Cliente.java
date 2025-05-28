b510ee33e4c467af6772d32f6f59a7e97e7e4e4f
João Vitor Capazório Domingos <joao.domingos.15@estudante.unespar.edu.br>
2025-05-28 09:35:33 -0300

    Arquivos Base
    Arquivos base para a construção do projeto

---
+++ Cliente/src/main/java/domingos/jv/cliente/Cliente.java
@@ -0,0 +1,8 @@
+package domingos.jv.cliente;
+
+public class Cliente {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Servidor/src/main/java/domingos/jv/servidor/Servidor.java
@@ -0,0 +1,8 @@
+package domingos.jv.servidor;
+
+public class Servidor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
