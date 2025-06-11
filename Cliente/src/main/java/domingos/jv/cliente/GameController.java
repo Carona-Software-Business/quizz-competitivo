fd1726f7e41d3220bda58be641d304c60c70520b
João Vitor Capazório Domingos <joao.domingos.15@estudante.unespar.edu.br>
2025-06-11 12:07:19 -0300

    Create GameController.java

---
+++ Cliente/src/main/java/domingos/jv/cliente/GameController.java
@@ -0,0 +1,40 @@
+package domingos.jv.cliente;
+
+import java.util.List;
+
+public class GameController {
+    private List<Pergunta> perguntas;
+    private int quantidadePerguntas;
+    private String dificuldadeAtual;
+
+    public GameController() {
+        
+    }
+    
+    public void reiniciarJogo() {
+        // Reiniciar as perguntas
+        // Reiniciar a dificuldade
+    }
+    
+    private void definirDificuldade() {
+        // Defini a dificuldade baseado nas quantidades de perguntas
+    }
+    
+    public Pergunta escolharPergunta() {
+        // Escolher a pergunta aleatóriamente dependendo da dificuldade
+        return null;
+    }
+    
+    public Boolean verificarResposta() {
+        // Verificar a resposta, define dificuldade.
+        return null;
+    }
+    
+    private void calcularPontos() {
+        
+    }
+    
+    
+    
+    
+}
