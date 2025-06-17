c7d97e660ac7688fdcc3edaa72fe6ebaa5bd33d5
Guilherme Henrique de Sousa <guilherme29sousa@outlook.com>
2025-06-17 19:17:15 -0300

    Commit das interfaces gráficas
    Criação das interfaces gráficas Inicio, Erro, Acerto, Pergunta, Final e a classe Principal que se encontra o main.
    Os botões também foram criados.

--- Cliente/src/main/java/domingos/jv/cliente/Acerto.java
+++ Cliente/src/main/java/domingos/jv/cliente/Acerto.java
@@ -1,13 +1,95 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package domingos.jv.cliente;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-/**
- *
- * @author guilh
- */
-public class Acerto {
+public class Acerto extends JFrame{
+    
+    JLabel tituloFinal;
+    JLabel pontuacaoFinal;
+    JPanel painelFinalCentro;
+    JPanel painelFinalEsquerda;
+    JPanel painelFinalDireita;
+    JPanel painelFinalNorte;
+    JPanel painelFinalSul;
+    JButton botaoProximo;
+    
+    public Acerto(){
+        
+        setLayout(new BorderLayout());
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice gd = ge.getDefaultScreenDevice();
+
+        if (gd.isFullScreenSupported()) {
+            setUndecorated(true);
+            gd.setFullScreenWindow(this);
+        } else {
+            setExtendedState(JFrame.MAXIMIZED_BOTH);
+        }
+        
+        tituloFinal = new JLabel("ACERTOU!");
+        pontuacaoFinal = new JLabel("Sua pontuação atual é: ");
+        painelFinalCentro = new JPanel(new GridLayout(2, 0));
+        painelFinalEsquerda = new JPanel(new FlowLayout());
+        painelFinalDireita = new JPanel(new GridBagLayout());
+        painelFinalNorte = new JPanel(new FlowLayout());
+        painelFinalSul = new JPanel(new FlowLayout());
+        botaoProximo = new JButton("PRÓXIMA PERGUNTA");
+        
+        botaoProximo.setPreferredSize(new Dimension(250, 100));
+        botaoProximo.setBackground(Color.WHITE);
+        botaoProximo.setForeground(Color.BLACK);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 3;
+        gbc.gridy = 0;
+        
+        painelFinalCentro.setBackground(Color.GREEN);
+        painelFinalDireita.setBackground(Color.GREEN);
+        painelFinalEsquerda.setBackground(Color.GREEN);
+        painelFinalNorte.setBackground(Color.GREEN);
+        painelFinalSul.setBackground(Color.GREEN);
+        
+        tituloFinal.setFont(new Font("Arial", Font.BOLD, 100));
+        tituloFinal.setForeground(Color.WHITE);
+        pontuacaoFinal.setFont(new Font("Arial", Font.BOLD, 50));
+        pontuacaoFinal.setForeground(Color.WHITE);
+        botaoProximo.setFont(new Font("Arial", Font.BOLD, 20));
+        botaoProximo.setVerticalAlignment(SwingConstants.CENTER);
+        botaoProximo.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
+        
+        painelFinalEsquerda.setPreferredSize(new Dimension(300, 0));
+        painelFinalDireita.setPreferredSize(new Dimension(300, 0));
+        painelFinalNorte.setPreferredSize(new Dimension(0, 100));
+        painelFinalSul.setPreferredSize(new Dimension(0, 100));
+        
+        painelFinalCentro.add(tituloFinal);
+        painelFinalCentro.add(pontuacaoFinal);
+        painelFinalDireita.add(botaoProximo, gbc);
+        
+        add(painelFinalCentro, BorderLayout.CENTER);
+        add(painelFinalEsquerda, BorderLayout.WEST);
+        add(painelFinalDireita, BorderLayout.EAST);
+        add(painelFinalSul, BorderLayout.SOUTH);
+        add(painelFinalNorte, BorderLayout.NORTH);
+
+        //Botao do proximo, apenas fazer a logica
+        ActionListener listener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource().equals(botaoProximo)){
+                   
+                }
+            }
+        };
+        
+        botaoProximo.addActionListener(listener);
+        
+        setVisible(true);
+    }
     
 }
--- Cliente/src/main/java/domingos/jv/cliente/Final.java
+++ Cliente/src/main/java/domingos/jv/cliente/Final.java
@@ -1,13 +1,82 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package domingos.jv.cliente;
+import javax.swing.*;
+import java.awt.*;
 
-/**
- *
- * @author guilh
- */
-public class Final {
+public class Final extends JFrame{
+    
+    JLabel tituloFinal;
+    JLabel pontuacaoFinal;
+    JPanel painelFinalCentro;
+    JPanel painelFinalEsquerda;
+    JPanel painelFinalDireita;
+    JPanel painelFinalNorte;
+    JPanel painelFinalSul;
+    JButton botaoProximo;
+    
+    public Final(){
+        
+        setLayout(new BorderLayout());
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice gd = ge.getDefaultScreenDevice();
+
+        if (gd.isFullScreenSupported()) {
+            setUndecorated(true);
+            gd.setFullScreenWindow(this);
+        } else {
+            setExtendedState(JFrame.MAXIMIZED_BOTH);
+        }
+        
+        tituloFinal = new JLabel("FIM DE JOGO!");
+        pontuacaoFinal = new JLabel("Sua pontuação final é: ");
+        painelFinalCentro = new JPanel(new GridLayout(2, 0));
+        painelFinalEsquerda = new JPanel(new FlowLayout());
+        painelFinalDireita = new JPanel(new GridBagLayout());
+        painelFinalNorte = new JPanel(new FlowLayout());
+        painelFinalSul = new JPanel(new FlowLayout());
+        botaoProximo = new JButton("PRÓXIMO JOGADOR");
+        
+        botaoProximo.setPreferredSize(new Dimension(250, 100));
+        botaoProximo.setBackground(Color.WHITE);
+        botaoProximo.setForeground(Color.BLACK);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 3;
+        gbc.gridy = 0;
+        
+        painelFinalCentro.setBackground(Color.YELLOW);
+        painelFinalDireita.setBackground(Color.YELLOW);
+        painelFinalEsquerda.setBackground(Color.YELLOW);
+        painelFinalNorte.setBackground(Color.YELLOW);
+        painelFinalSul.setBackground(Color.YELLOW);
+        
+        tituloFinal.setFont(new Font("Arial", Font.BOLD, 100));
+        tituloFinal.setForeground(Color.BLACK);
+        pontuacaoFinal.setFont(new Font("Arial", Font.BOLD, 50));
+        pontuacaoFinal.setForeground(Color.BLACK);
+        botaoProximo.setFont(new Font("Arial", Font.BOLD, 20));
+        botaoProximo.setVerticalAlignment(SwingConstants.CENTER);
+        botaoProximo.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
+        
+        painelFinalEsquerda.setPreferredSize(new Dimension(300, 0));
+        painelFinalDireita.setPreferredSize(new Dimension(300, 0));
+        painelFinalNorte.setPreferredSize(new Dimension(0, 100));
+        painelFinalSul.setPreferredSize(new Dimension(0, 100));
+        
+        painelFinalCentro.add(tituloFinal);
+        painelFinalCentro.add(pontuacaoFinal);
+        painelFinalDireita.add(botaoProximo, gbc);
+        
+        add(painelFinalCentro, BorderLayout.CENTER);
+        add(painelFinalEsquerda, BorderLayout.WEST);
+        add(painelFinalDireita, BorderLayout.EAST);
+        add(painelFinalSul, BorderLayout.SOUTH);
+        add(painelFinalNorte, BorderLayout.NORTH);
+
+
+        setVisible(true);
+    }
     
 }
